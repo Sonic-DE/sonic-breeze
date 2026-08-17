3956a9fa89ea44592ef92b7469ab8408a160b917
Joseph Crowell <joseph.w.crowell@gmail.com>
2026-08-17 21:34:17 +1000

    remove the gtk code that was mistakenly pulled in from breeze-gtk
    Signed-off-by: Joseph Crowell <joseph.w.crowell@gmail.com>

--- gtk/cmake/FindPythonCairo.cmake
+++
@@ -1,8 +0,0 @@
-find_package(Python3 COMPONENTS Interpreter REQUIRED)
-
-# Check for python cairo
-execute_process(COMMAND ${Python3_EXECUTABLE} -c "import cairo"
-                RESULT_VARIABLE PYTHON_CAIRO_RESULT)
-if (PYTHON_CAIRO_RESULT EQUAL 0)
-    set(PYTHONCAIRO_FOUND 1)
-endif()
